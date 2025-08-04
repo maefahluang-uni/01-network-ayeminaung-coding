3a9d8d53099370228b4d730a1ee2e7fec8b3d57c
ayeminaung-coding <6731503047@lamduan.mfu.ac.th>
2025-08-04 09:24:59 +0000

    lab network

--- src/main/java/th/mfu/MockWebClient.java
+++ src/main/java/th/mfu/MockWebClient.java
@@ -8,11 +8,31 @@ public class MockWebClient {
     public static void main(String[] args) {
 
         // TODO: Create a socket to connect to the web server on port 8080
+        try (Socket socket = new Socket("localhost", 8080)) {
+            System.out.println("Connected to Mock Web Server on port 8080...");
+            String request = "GET / HTTP/1.1\r\nHost: localhost\r\n\r\n";
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+
+            out.println(request);
+
+            String userInput;
+            while ((userInput = stdIn.readLine()) != null) {
+                System.out.println("From server response : " + userInput); 
+            }
+
+            socket.close();
+            System.out.println("Connection closed.");
+
+        } catch (IOException e) {
+            System.err.println("Could not connect to server: " + e.getMessage());
+            return;
+        }
 
         // :TODO Create input and output streams for the socket
 
         // TODO: send an HTTP GET request to the web server
-        String request = "GET / HTTP/1.1\r\nHost: localhost\r\n\r\n";
 
         // Read the response from the web server and print out to console
 
--- src/main/java/th/mfu/MockWebServer.java
+++ src/main/java/th/mfu/MockWebServer.java
@@ -15,22 +15,35 @@ public class MockWebServer implements Runnable {
     public void run() {
 
         // TODO Create a server socket bound to specified port
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
 
-        System.out.println("Mock Web Server running on port " + port + "...");
+            System.out.println("Mock Web Server running on port " + port + "...");
 
-        while (true) {
-            // TODO Accept incoming client connections
+            while (true) {
+                // TODO Accept incoming client connections
+                Socket clientSocket = serverSocket.accept();
 
-            // TODO Create input and output streams for the client socket
+                // TODO Create input and output streams for the client socket
+                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                // TODO: Read the request from the client using BufferedReader
+                System.out.println(in.readLine());
+                System.out.println(in.readLine());
+                System.out.println(in.readLine());
+                System.out.println(in.readLine());
 
-            // TODO: Read the request from the client using BufferedReader
+                // TODO: send a response to the client
+                String response = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n"
+                        + "<html><body>Hello, Web! on Port " + port + "</body></html>";
+                out.println(response);
 
-            // TODO: send a response to the client
-            String response = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n"
-                    + "<html><body>Hello, Web! on Port " + port + "</body></html>";
+                // TODO: Close the client socket
+                clientSocket.close();
 
-            // TODO: Close the client socket
-
+            }
+        } catch (IOException e) {
+            System.err.println("Could not start server on port " + port + ": " + e.getMessage());
+            return;
         }
 
     }
